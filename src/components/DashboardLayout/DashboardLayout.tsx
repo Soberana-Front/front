8d8286a24101fe48d802cf147e3fd30fe90f0746
enzzodasul <enzzodasul@gmail.com>
2026-08-24 20:25:58 -0300

    teste e correção adição header na dashboard leyout

--- src/components/DashboardLayout/DashboardLayout.tsx
+++ src/components/DashboardLayout/DashboardLayout.tsx
@@ -1,4 +1,6 @@
 import type { ReactNode } from 'react'
+import Header from '../Header/Header';
+import { Sidebar } from '../Sidebar/Sidebar';
 
 type DashboardLayoutProps = {
   children: ReactNode
@@ -11,12 +13,12 @@ export function DashboardLayout({
     <div className="min-h-screen bg-slate-50 text-slate-950">
       <div className="flex min-h-screen">
         <aside className="hidden w-[240px] shrink-0 border-r border-slate-200 bg-white lg:block">
-          {/* Sidebar */}
+           <Sidebar />
         </aside>
 
         <div className="flex min-w-0 flex-1 flex-col">
           <header className="h-14 shrink-0 border-b border-slate-200 bg-white">
-            {/* Header */}
+            <Header />
           </header>
 
           <main className="min-w-0 flex-1 p-6">
