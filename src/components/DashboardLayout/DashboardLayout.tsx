f8669525dff58a4b4d2de4ab679578422f4137bf
enzzodasul <enzzodasul@gmail.com>
2026-08-24 21:32:55 -0300

    correção na sidebar que agora esta rolando independente

--- src/components/DashboardLayout/DashboardLayout.tsx
+++ src/components/DashboardLayout/DashboardLayout.tsx
@@ -1,31 +1,33 @@
-import type { ReactNode } from 'react'
+import type { ReactNode } from 'react';
 import Header from '../Header/Header';
 import { Sidebar } from '../Sidebar/Sidebar';
 
-type DashboardLayoutProps = {
-  children: ReactNode
+interface DashboardLayoutProps {
+  children: ReactNode;
 }
 
-export function DashboardLayout({
-  children,
-}: DashboardLayoutProps) {
+export function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
-    <div className="min-h-screen bg-slate-50 text-slate-950">
-      <div className="flex min-h-screen">
-        <aside className="hidden w-[240px] shrink-0 border-r border-slate-200 bg-white lg:block">
-           <Sidebar />
-        </aside>
-
-        <div className="flex min-w-0 flex-1 flex-col">
-          <header className="h-14 shrink-0 border-b border-slate-200 bg-white">
-            <Header />
-          </header>
-
-          <main className="min-w-0 flex-1 p-6">
-            {children}
-          </main>
+    <div className="min-h-screen bg-slate-50 flex">
+      {/* Sidebar */}
+      <aside className="hidden lg:flex lg:w-60 lg:flex-col lg:shrink-0 border-r border-slate-200 bg-white">
+        <div className="sticky top-0 h-screen overflow-y-auto">
+          <Sidebar />
         </div>
+      </aside>
+
+      {/* Conteúdo principal */}
+      <div className="min-w-0 flex-1 flex flex-col">
+        {/* Header fixo/sticky */}
+        <div className="sticky top-0 z-40 shrink-0">
+          <Header />
+        </div>
+
+        {/* Conteúdo da página */}
+        <main className="min-w-0 flex-1">
+          {children}
+        </main>
       </div>
     </div>
-  )
+  );
 }
